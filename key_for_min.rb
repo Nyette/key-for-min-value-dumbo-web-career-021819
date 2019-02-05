b47cb51b42f349b07cc2f8fc151d8ad94ccc4ef2
Antoinette Lee <antoinette.lee02@myhunter.cuny.edu>
2019-02-05 09:17:03 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,11 +1,14 @@
-# prereqs: iterators, hashes, conditional logic
+# Pre-Reqs: iterators, hashes, conditional logic
 # Given a hash with numeric values, return the key for the smallest value
+
+require "pry"
 
 def key_for_min_value(name_hash)
   if name_hash = {}
     nil
   else
     name_hash_array = name_hash.to_a.flatten(2)
+    binding.pry
     min_value = name_hash_array[1]
     min_key = name_hash_array[0]
     name_hash.each do |key, value|
